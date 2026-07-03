f53adb5faf3c1c5a432b729d09e8e31a0e421bbe
MonikaR1990 <monicaselas2010@gmail.com>
2026-07-03 18:48:16 +0530

    Added Google Playwright test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -5,4 +5,6 @@ test('example test', async ({ page }) =>
   await page.goto('https://google.com');
   await expect(page).toHaveTitle(/Google/);
   console.log("Hello Google")
+  console.log("Hello Google! How are you doing today?");
+  console.log("Hello Google! How are you doing today? I hope you are having a great day!");
 });
